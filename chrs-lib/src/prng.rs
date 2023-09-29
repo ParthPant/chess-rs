112226bf9a76df23eb4dd077393ea49fdc3b718e
Mike Dallas <mcdallas@protonmail.com>
2023-09-29 17:52:59 +0100

    replace chacha prng

--- chrs-lib/src/prng.rs
+++ chrs-lib/src/prng.rs
@@ -1,9 +1,8 @@
 use rand::prelude::*;
-use rand_chacha::ChaCha20Rng;
 use std::cell::RefCell;
 
 thread_local! {
-    pub static PRNG: RefCell<ChaCha20Rng> = RefCell::new(ChaCha20Rng::seed_from_u64(234239432));
+    pub static PRNG: RefCell<ThreadRng> = RefCell::new(thread_rng());
 }
 
 pub fn random_u64() -> u64 {
